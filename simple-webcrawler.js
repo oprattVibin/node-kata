b57b9b24387f441968b7434ce5bb8bcc3cdeaa1e
Vinay Vyas <vbvyas@gmail.com>
2014-01-11 00:29:44 -0800

    Add cheerio

--- simple-webcrawler.js
+++ simple-webcrawler.js
@@ -1,5 +1,6 @@
 var program = require('commander');
 var req = require('request');
+var cheerio = require('cheerio');
 
 program
   .version('0.0.1')
@@ -10,6 +11,8 @@ request(program.url, function(err, res, body) {
   if (err) console.log(err);
   else {
     // read page
+    $ = cheerio.load(body);
     // get all anchors using cheerio
+    var anchors = $('a').attr('href');
   }
 });
