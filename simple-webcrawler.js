2d23d9cf2461c689c4beea993259daf979c5760f
Vinay Vyas <vbvyas@gmail.com>
2014-01-11 00:53:40 -0800

    Update simple-webcrawler.js

--- simple-webcrawler.js
+++ simple-webcrawler.js
@@ -6,8 +6,11 @@ program
   .version('0.0.1')
   .option('-u, --url', 'Root url to crawl')
   .parse(process.argv);
+  
+var visited = {};
 
 function crawl(url) {
+  if (visited[url]) return;
   request(url, function(err, res, body) {
     if (err) console.log(err);
     else {
@@ -16,8 +19,12 @@ function crawl(url) {
       // get all anchors using cheerio
       var anchors = $('a').attr('href');
       anchors.forEach(crawl);
+      anchors.forEach(function (u) {
+        visited[u] = true;
+      });
     }
   });
 }
 
+visited[program.url] = true;
 crawl(program.url);
