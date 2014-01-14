c80be6b4e55f90e9a4d9d389625543707067839a
Vinay Vyas <vbvyas@gmail.com>
2014-01-13 17:08:25 -0800

    Update consume-rest-api.js

--- consume-rest-api.js
+++ consume-rest-api.js
@@ -1,3 +1,16 @@
 // simple node.js example
 // to consume REST api
 
+var http = require('http');
+var util = require('util');
+
+var url = process.argv[2];
+
+http.get(url, function(res) {
+  var data = '';
+  res.on('data', function (body){
+    data += body;
+    console.log(data) // print JSON data
+    var obj = JSON.parse(data);
+  });
+});
