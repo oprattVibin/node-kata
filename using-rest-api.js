9ea3f1e1619c65f8589886d79bd1494f0007163d
Vinay Vyas <vbvyas@gmail.com>
2014-01-14 10:55:29 -0800

    Update using-rest-api.js

--- using-rest-api.js
+++ using-rest-api.js
@@ -1,8 +1,18 @@
-# Uses the request api https://github.com/mikeal/request
+// Uses the request api https://github.com/mikeal/request
 
 var request = require('request');
 
-var post_data = function() {
+var post_data = function(url, json) {
+  // request.post(url, json);
+  var options = {
+    uri: url,
+    method: 'POST',
+    json: json
+  };
+  request(options, function(err, res, body) {
+    if (err) console.log(err);
+    else console.log(body);
+  });
 };
 
 var get_data = function() {
