195105fb02a3810878c0ab34b5679c2eaab9d3c3
Vinay Vyas <vbvyas@gmail.com>
2014-01-13 17:20:47 -0800

    Create using-rest-api.js

---
+++ using-rest-api.js
@@ -0,0 +1,15 @@
+# Uses the request api https://github.com/mikeal/request
+
+var request = require('request');
+
+var post_data = function() {
+};
+
+var get_data = function() {
+};
+
+var post_with_auth = function() {
+};
+
+var get_with_auth = function() {
+};
