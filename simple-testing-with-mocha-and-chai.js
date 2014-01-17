3132febf1ba18357f92035dabbcabee32e778921
Vinay Vyas <vbvyas@gmail.com>
2014-01-17 00:17:57 -0800

    Update simple-testing-with-mocha-and-chai.js

--- simple-testing-with-mocha-and-chai.js
+++ simple-testing-with-mocha-and-chai.js
@@ -7,12 +7,24 @@
 var should = require('chai').should();
 
 describe('Array', function() {
+  before(function() {
+    var array = [1, 2, 3];
+  });
+  
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      var array = [1, 2, 3];
       array.indexOf(5).should.equal(-1);
+    });
+  });
+  
+  describe('#length', fucntion() {
+    it('should return the exact length of elements', function() {
       array.should.have.length(3);
-      array.indexOf(3).should.equal(2);
+    });
+  });
+  
+  describe('type of element', function() {
+    it('should have elements that are numbers', function() {
       array[0].should.be.a('Number');
     });
   });
