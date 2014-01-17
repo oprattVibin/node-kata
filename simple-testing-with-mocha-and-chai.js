0977f57e5410b6608275569131a3d9b3e3eb970a
Vinay Vyas <vbvyas@gmail.com>
2014-01-17 00:09:16 -0800

    Update simple-testing-with-mocha-and-chai.js

--- simple-testing-with-mocha-and-chai.js
+++ simple-testing-with-mocha-and-chai.js
@@ -1,1 +1,20 @@
-//
+// Install mocha
+// npm install -g mocha
+// Install chai
+// npm install chai
+// To run just tpye mocha on the terminal
+
+var should = require('chai').should();
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      var array = [1, 2, 3];
+      array.indexOf(5).should.equal(-1);
+      array.should.have.length(3);
+      array.indexOf(3).should.equal(2);
+      array[0].should.be.a('Number');
+    });
+  });
+});
+
