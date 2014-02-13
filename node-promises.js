7b3a350d6b38ae7ba9170188c7aff9d05f4eefa2
Vinay Vyas <vbvyas@gmail.com>
2014-02-12 23:01:25 -0800

    Added a simple deferred example.

--- node-promises.js
+++ node-promises.js
@@ -2,3 +2,32 @@
 // For detailed examples: http://documentup.com/kriskowal/q
 // Using promises is good if there are nested callbacks
 // or things need to be done sequentially.
+
+// Example if your code looks something like this
+function one(arg1, callback) {
+  // Do something
+  callback(return_value);
+}
+
+// Inorder to access the return_value
+function two(arg1) {
+  one(arg1, function(result){
+    // Do something with the result
+  }
+}
+
+// With Q.js
+var Q = require('q');
+
+function one(arg1, callback) {
+  var deferred = Q.defer();
+  
+  // Do something
+  
+  return deferred.promise.nodeify(callback);
+}
+
+// In order to get the result no need to pass a callback
+function two(arg1) {
+  var result = one(arg1);
+}
