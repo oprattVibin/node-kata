8af8ef4e968c7f65ddd3c670e13606889b8adb4b
Vinay Vyas <vbvyas@gmail.com>
2014-02-12 22:04:48 -0800

    Update node-promises.js

--- node-promises.js
+++ node-promises.js
@@ -1,2 +1,4 @@
 // Q.js example to show how to use promises
 // For detailed examples: http://documentup.com/kriskowal/q
+// Using promises is good if there are nested callbacks
+// or things need to be done sequentially.
