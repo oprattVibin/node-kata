7ae3b00d4047960ecf1bdffefdb4ef1105e31a7f
Vinay Vyas <vbvyas@gmail.com>
2014-03-26 12:04:56 -0700

    Update recommend.js

--- recommend.js
+++ recommend.js
@@ -1,2 +1,10 @@
 // Simple recommendation system in node
 // Inspiration from: http://otobrglez.opalab.com/ruby/2014/03/23/simple-ruby-recommendation-system.html
+
+// Read book titles from a file (create the corpus)
+function createCorpus() {
+}
+
+// Based on a book title, recommend other titles
+function recommend(title) {
+}
