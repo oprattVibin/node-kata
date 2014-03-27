1136051acceeb6d64eeca89c14c7cb77bc395319
Vinay Vyas <vbvyas@gmail.com>
2014-03-27 11:39:21 -0700

    Update recommend.js

--- recommend.js
+++ recommend.js
@@ -2,7 +2,16 @@
 // Inspiration from: http://otobrglez.opalab.com/ruby/2014/03/23/simple-ruby-recommendation-system.html
 
 // Read book titles from a file (create the corpus)
-function createCorpus() {
+function createCorpus(path) {
+  var fs = require('fs');
+  
+  fs.readFile(path, 'utcf8', function (err, data){
+    if (err) {
+      return console.log(err);
+    }
+    
+    // Read file line by line
+  });
 }
 
 // Based on a book title, recommend other titles
