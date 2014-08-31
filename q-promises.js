5e982387278cc9f1af7f2c860ca860218ba21f9f
Vinay Vyas <vbvyas@gmail.com>
2014-08-31 00:01:34 -0700

    Update q-promises.js

--- q-promises.js
+++ q-promises.js
@@ -1,1 +1,17 @@
-# promises in node.js
+// promises in node.js
+
+var q = require('q');
+
+// method 1
+var readFile = q.node(fs.readFile);
+readFile('test.txt')
+.then(function (data) { console.log(data); });
+
+// method 2
+q.ncall(fs.readFile, fs, 'test.txt')
+.then(function (data) { console.log(data); });
+
+// method 3
+var deferred = q.defer();
+fs.readFile('text.txt', deferred.node());
+return deferred.promise;
