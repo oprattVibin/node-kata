528790716223b64f05563097625e73993c6b6557
Vinay Vyas <vbvyas@gmail.com>
2014-09-06 12:44:10 -0700

    Update consume-rest-json.js

--- consume-rest-json.js
+++ consume-rest-json.js
@@ -1,15 +1,15 @@
 // simple node.js example
 // to consume REST api
 
-var http = require('http');
+var http = require('http'); // for https use require('https')
 var util = require('util');
 
-var url = process.argv[2];
+var url = process.argv[2]; // read url from stdin
 
 http.get(url, function(res) {
   var data = '';
   res.on('data', function (body){
-    data += body;
+    data += body; // essentially converts binary to string
     console.log(data) // print JSON data
     var obj = JSON.parse(data);
   });
