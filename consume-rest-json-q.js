a770da7855ad0a12d6ea91e80dde11b397ba36a9
Vinay Vyas <vbvyas@gmail.com>
2014-09-08 21:34:09 -0700

    Update consume-rest-json-q.js

--- consume-rest-json-q.js
+++ consume-rest-json-q.js
@@ -8,16 +8,12 @@ var q = require('q');
 // var url = process.argv[2]; // read url from stdin
 var url = 'http://time.jsontest.com/';
 
-function result(res) {
-  var deferred = q.defer();
-  res.on('data', deferred.node());
-  return deferred.promise;
-}
-
 var get = q.denodeify(http.get);
-get(url).then(result).then(function (body) {
-  var data = '';
-  data += body;
-  console.log(data);
-  var obj = JSON.parse(data);
+get(url).then(function (res) {
+  res.on('data', function (body) {
+    var data = '';
+    data += body;
+    console.log(data);
+    var obj = JSON.parse(data);
+  })
 });
