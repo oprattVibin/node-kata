e1608bda22548838fba47572c2d919d2f9304843
Vinay Vyas <vbvyas@gmail.com>
2014-09-11 21:39:43 -0700

    Update map-reduce.js

--- map-reduce.js
+++ map-reduce.js
@@ -1,1 +1,7 @@
-# js map and reduce example
+// js map and reduce example
+
+var result = [1, 2, 3, 4].map(sqr).reduce(sum);
+console.log(result);
+
+function sqr(x) { return x * x; }
+function sum(a, b) { return a * b; }
