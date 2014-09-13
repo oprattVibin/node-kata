eb3bdecfe95e2322032526a43b0cd1709981c470
Vinay Vyas <vbvyas@gmail.com>
2014-09-12 18:57:28 -0700

    Update map-reduce.js

--- map-reduce.js
+++ map-reduce.js
@@ -1,7 +1,6 @@
 // js map and reduce example
+function sqr(x) { return x * x; }
+function sum(a, b) { return a + b; }
 
 var result = [1, 2, 3, 4].map(sqr).reduce(sum);
 console.log(result);
-
-function sqr(x) { return x * x; }
-function sum(a, b) { return a + b; }
