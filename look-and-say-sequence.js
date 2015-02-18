b32dd2b433f5a791a8a9d45a3540f51ecf4da439
Vinay Vyas <vbvyas@gmail.com>
2015-02-17 23:50:04 -0800

    Update look-and-say-sequence.js

--- look-and-say-sequence.js
+++ look-and-say-sequence.js
@@ -3,23 +3,23 @@ function look(n) {
     return "1";
   }
 
-  if (n == 1) {
-    return "11";
-  }
-
   var p = look(n - 1),
   c = p[0],
   count = 1,
-  result;
+  result = '';
 
   for (var i = 1; i < p.length; i++) {
     if (p[i] == c) {
       count++;
     } else {
-      result += c + count;
+      result += count + c;
       c = p[i];
       count = 1;
     }
   }
-  return result;
+  return result + count + c;
+}
+
+for (var i = 0; i <= 10; i++) {
+  console.log(i + " => " + look(i));
 }
