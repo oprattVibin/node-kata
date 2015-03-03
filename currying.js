d0b16f91344deaaa9192fe69ab6e1189d4eaf23a
Vinay Vyas <vbvyas@gmail.com>
2015-03-02 23:56:54 -0800

    Update currying.js

--- currying.js
+++ currying.js
@@ -1,6 +1,6 @@
-// http://jsfiddle.net/owv4Lcrs/
+http://jsfiddle.net/owv4Lcrs/1/
 
-function add1(a, b) {
+function add(a, b) {
     if (typeof b == 'undefined') {
         return function (x) {
             return a + x;
@@ -9,7 +9,7 @@ function add1(a, b) {
     return a + b;
 }
 
-function add2(a, b) {
+function _add(a, b) {
     if (arguments.length < 1) {
         return _add;
     }
@@ -29,6 +29,16 @@ function add3(a) {
     }
 }
 
+function add4() {
+    return function(a) {
+        return function(b) {
+            return a + b;
+        }
+    }
+}
+
 console.log(add(2, 5));
 console.log(add(2)(5));
 console.log(_add()(2)(5));
+console.log(add3(2)(5));
+console.log(add4()(2)(5));
